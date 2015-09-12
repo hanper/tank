3d1b358af0be681a161c70d629949e863f22de6d
Temuulen <songodog@gmail.com>
2015-09-12 22:32:21 +0900

    -- need to fix sudden death BUG

--- tank.py
+++ tank.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+import math
 
 pygame.init()
 #MESSAGE
@@ -36,9 +37,25 @@ head_width = 10
 head_height = 10
 speed = 10
 block_size = 10
+top_radius = 10
+bottom_radius = 30
+turret_wall = 0
+turret_head_length = 40
+turret_head_thickness = 5
 
+#TURRET
+def turret(center_x, center_y, lead_x, lead_y, enemy_b_p_x, enemy_b_p_y):
+    end_x = (turret_head_length) * ((lead_x - center_x) / math.sqrt(math.pow(lead_x - center_x, 2) + math.pow(lead_y - center_y, 2))) + center_x
+    end_y = (turret_head_length) * ((lead_y - center_y) / math.sqrt(math.pow(lead_x - center_x, 2) + math.pow(lead_y - center_y, 2))) + center_y
+    pygame.draw.circle(display, green, (center_x, center_y), bottom_radius, turret_wall)
+    turret_head(center_x, center_y, end_x, end_y)
+    pygame.draw.circle(display, blue, (center_x, center_y), top_radius, turret_wall)
+    bullet(enemy_b_p_x, enemy_b_p_y)
+#TURRET_HEAD
+def turret_head(center_x, center_y, end_x, end_y):
+    pygame.draw.line(display, black, (center_x, center_y), (end_x, end_y), turret_head_thickness)
 #TANK
-def tank(lead_x, lead_y, tank_width, tank_height, head_position_x, head_position_y, head_width, head_height):
+def tank(lead_x, lead_y, tank_width, tank_height, head_position_x, head_position_y):
     pygame.draw.rect(display, red, [lead_x, lead_y, tank_width, tank_height])
     pygame.draw.rect(display, blue, [lead_x + head_position_x, lead_y + head_position_y, head_width, head_height])
 #BULLET
@@ -53,12 +70,30 @@ def isHit(enemy_x, enemy_y, bullet_x, bullet_y):
     if bullet_x >= enemy_x and bullet_x <= enemy_x + tank_width and bullet_y >= enemy_y and bullet_y <= enemy_y + tank_height:
         enemy_hit = True
     return enemy_hit
-    
+#TURRET&BULLET collision detector
+def isTurretHit(center_x, center_y, bullet_x, bullet_y):
+    turret_hit = False
+    if bullet_x >= center_x - bottom_radius and bullet_x <= center_x + bottom_radius:
+        if bullet_y >= center_y - bottom_radius and bullet_y <= center_y + bottom_radius:
+            turret_hit = True
+    elif bullet_x + block_size >= center_x - bottom_radius and bullet_x + block_size <= center_x + bottom_radius:
+        if bullet_y + block_size >= center_y - bottom_radius and bullet_y + block_size <= center_y + bottom_radius:
+            turret_hit = True
+    return turret_hit
+#TANK&BULLET collision detector
+def isTankHit(lead_x, lead_y, bullet_x, bullet_y):
+    tank_hit = False
+    if bullet_x >= lead_x and lead_x <= lead_x + tank_width:
+        if bullet_y >= lead_y and bullet_y <= lead_y + tank_height:
+            tank_hit = True
+    elif bullet_x + block_size >= lead_x and bullet_x + block_size <= lead_x + tank_width:
+        if bullet_y + block_size >= lead_y and bullet_y + block_size <= lead_y + tank_height:
+            tank_hit = True
+    return tank_hit
 #SCREEN text 
 def message_to_screen(msg, color):
     screen_text = font.render(msg, True, color)
     display.blit(screen_text, [window_width / 2, window_height / 2])
-
 #GAME loop
 def gameLoop():
     #EXIT value
@@ -80,16 +115,19 @@ def gameLoop():
     bullet_y_change = 0
     bullet_speed = 10
 
+    enemy_bullet_x = 0
+    enemy_bullet_y = 0
+    enemy_b_p_x = -10
+    enemy_b_p_y = -10
+
     enemy_x = round( random.randrange(0, window_width - tank_width) / 10.0 ) * 10.0
     enemy_y = round( random.randrange(0, window_height - tank_height) / 10.0 ) * 10.0
 
+    center_x = random.randrange(turret_head_length, window_width - turret_head_length)
+    center_y = random.randrange(turret_head_length, window_height - turret_head_length)
 ##    enemy_x_change = round( random.randrange(-10, 10) / 10.0 ) * 10.0
 ##    enemy_y_change = round( random.randrange(-10, 10) / 10.0 ) * 10.0
-    
-    #SNAKE list
-    snakeList = []
-    snakeLength = 1
-    
+
     while not gameExit:
         while gameOver == True:
             display.fill(white)
@@ -104,11 +142,25 @@ def gameLoop():
                     if event.key == pygame.K_r:
                         gameLoop()
                     
-            
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameExit = True
             if event.type == pygame.KEYDOWN:
+                enemy_b_p_x = (turret_head_length) * ((lead_x - center_x) / math.sqrt(math.pow(lead_x - center_x, 2) + math.pow(lead_y - center_y, 2))) + center_x
+                enemy_b_p_y = (turret_head_length) * ((lead_y - center_y) / math.sqrt(math.pow(lead_x - center_x, 2) + math.pow(lead_y - center_y, 2))) + center_y
+                if center_x < lead_x and center_y < lead_y:
+                    enemy_bullet_x = bullet_speed
+                    enemy_bullet_y = bullet_speed / (enemy_b_p_x - center_x) * ( enemy_b_p_y - center_y)
+                elif center_x < lead_x and center_y > lead_y:
+                    enemy_bullet_x = bullet_speed
+                    enemy_bullet_y = -bullet_speed / (enemy_b_p_x - center_x) * ( -enemy_b_p_y + center_y)
+                elif center_x > lead_x and center_y < lead_y:
+                    enemy_bullet_x = -bullet_speed
+                    enemy_bullet_y = bullet_speed / (-enemy_b_p_x + center_x) * ( enemy_b_p_y - center_y)
+                elif center_x > lead_x and center_y > lead_y:
+                    enemy_bullet_x = -bullet_speed
+                    enemy_bullet_y = -bullet_speed / (-enemy_b_p_x + center_x) * ( -enemy_b_p_y + center_y)
+
                 if event.key == pygame.K_LEFT:
                     lead_x_change = -speed
                     head_position_x = -10
@@ -142,6 +194,7 @@ def gameLoop():
                         bullet_x_change = -bullet_speed
                     elif head_direction == 3:
                         bullet_y_change = bullet_speed
+
                     
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -149,27 +202,40 @@ def gameLoop():
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     lead_y_change = 0
 
-        if lead_x >= window_width - (tank_width + head_width) or lead_x < 0 + (tank_width) or lead_y >= window_height - (tank_height + head_height) or lead_y < 0 + (tank_height):
-                
+        if lead_x >= window_width - (tank_width + head_width) or lead_x < 0 + (tank_width) or lead_y >= window_height - (tank_height + head_height) or lead_y < 0 + (tank_height):  
             gameOver = True
 
         lead_x += lead_x_change
         lead_y += lead_y_change
-        enemy_x += enemy_x_change
-        enemy_y += enemy_y_change
+##        enemy_x += enemy_x_change
+##        enemy_y += enemy_y_change
         bullet_position_x += bullet_x_change
         bullet_position_y += bullet_y_change
 
+        enemy_b_p_x += enemy_bullet_x
+        enemy_b_p_y += enemy_bullet_y
+        
         display.fill(white)
         bullet(bullet_position_x, bullet_position_y)
-        tank(lead_x, lead_y, tank_width, tank_height, head_position_x, head_position_y, head_width, head_height)
-        enemy(enemy_x, enemy_y)
-        enemy_state = isHit(enemy_x, enemy_y, bullet_position_x, bullet_position_y)
-
-        if enemy_state:
-            enemy_x = round( random.randrange(0, window_width - tank_width) / 10.0 ) * 10.0
-            enemy_y = round( random.randrange(0, window_height - tank_height) / 10.0 ) * 10.0
-            enemy(enemy_x, enemy_y)
+        tank(lead_x, lead_y, tank_width, tank_height, head_position_x, head_position_y)
+        #enemy(enemy_x, enemy_y)
+        turret(center_x, center_y, lead_x, lead_y, enemy_b_p_x, enemy_b_p_y)
+        turret_state = isTurretHit(center_x, center_y, bullet_position_x, bullet_position_y)
+        tank_state = isTankHit(lead_x, lead_y, enemy_b_p_x, enemy_b_p_y)
+        
+        if turret_state:
+            center_x = random.randrange(0, window_width - turret_head_length)
+            center_y = random.randrange(0, window_height - turret_head_length)
+            turret(center_x, center_y, lead_x, lead_y, enemy_b_p_x, enemy_b_p_y)
+        if tank_state:
+            gameOver = True
+            
+##        enemy_state = isHit(enemy_x, enemy_y, bullet_position_x, bullet_position_y)
+##
+##        if enemy_state:
+##            enemy_x = round( random.randrange(0, window_width - tank_width) / 10.0 ) * 10.0
+##            enemy_y = round( random.randrange(0, window_height - tank_height) / 10.0 ) * 10.0
+##            enemy(enemy_x, enemy_y)
 
         #print(enemy_state)
         
